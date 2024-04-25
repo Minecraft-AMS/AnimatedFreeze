4915d115497cf10ad1aed4a6d986490062a831e9
1024_byteeeee <2270484921@qq.com>
2024-04-25 21:19:10 +0800

    :white_check_mark: 默认开启资源包

--- src/main/java/top/byteeeee/AnimatedFreeze/AnimatedFreeze.java
+++ src/main/java/top/byteeeee/AnimatedFreeze/AnimatedFreeze.java
@@ -1,3 +1,23 @@
+/*
+ * This file is part of the AnimatedFreeze project, licensed under the
+ * GNU Lesser General Public License v3.0
+ *
+ * Copyright (C) 2024 1024_byteeeee and contributors
+ *
+ * AnimatedFreeze is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AnimatedFreeze is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with AnimatedFreeze. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package top.byteeeee.AnimatedFreeze;
 
 import net.fabricmc.api.ModInitializer;
@@ -19,12 +39,12 @@ public class AnimatedFreeze implements ModInitializer {
 		LOGGER.info(modName + " " + "loaded!");
 
 		FabricLoader.getInstance().getModContainer("animatedfreeze").ifPresent(
-				modContainer ->
-				ResourceManagerHelper.registerBuiltinResourcePack(
-						new Identifier("animatedfreeze:animatedfreeze"),
-						modContainer,
-						ResourcePackActivationType.NORMAL
-				)
+			modContainer ->
+			ResourceManagerHelper.registerBuiltinResourcePack(
+				new Identifier("animatedfreeze:animatedfreeze"),
+				modContainer,
+				ResourcePackActivationType.DEFAULT_ENABLED
+			)
 		);
 	}
 }
